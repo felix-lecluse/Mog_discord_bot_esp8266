e1436adc2ae483ecde010e042d1bff5968c8359f
felix-lecluse <62310117+felix-lecluse@users.noreply.github.com>
2020-10-29 12:48:34 +0100

    Add Credits

--- Mog_discord_bot_esp8266.ino
+++ Mog_discord_bot_esp8266.ino
@@ -4,9 +4,11 @@
 */
 #include "discord.h"
 /*
- * Webpage.h and text.h added by Félix Lecluse
- * Modify the text in text.h
-*/
+ * discord.h is made by Rémi Sarrailh aka maditnerd and David Ladd aka drencorxeendrencorxeen
+ * Link is here : https://github.com/maditnerd/discord_test
+ * webpage.h and text.h added by Félix Lecluse
+ * Modify the text in text.h and erase the // on the line "sendDiscord(Text);" in this file.
+ */
 #include "text.h"
 #include "webpage.h"
 
